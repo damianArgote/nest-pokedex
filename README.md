a02b05f0b84791fe309e0b75641db60a3b714927
Damián Argote <DArgote@coasa.com.ar>
2024-08-17 23:26:26 -0300

    paginacion terminada

--- README.md
+++ README.md
@@ -19,6 +19,11 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Poblar la db con el siguiente endpoint en desarrollo
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack
 * MongoDB
 * Nest
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -14,6 +14,7 @@ import { Pokemon, pokemonSchema } from './entities/pokemon.entity';
         schema: pokemonSchema
       }
     ])
-  ]
+  ],
+  exports:[MongooseModule]
 })
 export class PokemonModule {}
